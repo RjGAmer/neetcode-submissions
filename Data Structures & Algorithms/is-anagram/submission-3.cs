e8c2636b5acd6b746aa77cc02d34ef1b8ffdae31
Shayan Joardar <122792804+RjGAmer@users.noreply.github.com>
2026-05-15 08:48:00 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) {
+            return false;
+        }
+        char[] arr1=s.ToCharArray();
+        char[] arr2=t.ToCharArray();
+        Array.Sort(arr1);
+        Array.Sort(arr2);
+        if(new string(arr1) == new string(arr2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
